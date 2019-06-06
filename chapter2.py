4df82f571f5f38fadc572b49015eab85a5577a83
Vahid-Esmaeelzadeh <v.esmaeelzadeh@gmail.com>
2019-06-06 15:08:17 -0700

    The first part of Question 2.5 is solved (two methods: iterative and recursive)

--- chapter2.py
+++ chapter2.py
@@ -18,7 +18,9 @@ class linkedList:
             current = current.next
 
         current.next = item
-
+    def add(self, item):
+        item.next = self.head
+        self.head = item
     def remove(self):
         if self.head is not None:
             current = self.head
@@ -200,4 +202,85 @@ partition(copy.deepcopy(ll), -100).print()
 partition(copy.deepcopy(ll), 100).print()
 partition(copy.deepcopy(ll), 4).print()
 
+# endregion
+
+# region Question 2.5 (Sum lists)
+def sumLists(l1: linkedList, l2:linkedList):
+    i1 = l1.head
+    i2 = l2.head
+    result = linkedList()
+    carry = 0
+
+    while (i1 is not None) and (i2 is not None):
+        sum_val = i1.data + i2.data + carry
+        s = sum_val % 10
+        carry = sum_val // 10  # integer division
+        result.addLast(node(s))
+        i1 = i1.next
+        i2 = i2.next
+
+    if i1 is None:
+        while i2 is not None:
+            sum_val = i2.data + carry
+            s = sum_val % 10
+            carry = sum_val // 10
+            result.addLast(node(s))
+            i2 = i2.next
+
+    if i2 is None:
+        while i1 is not None:
+            sum_val = i1.data + carry
+            s = sum_val % 10
+            carry = sum_val // 10
+            result.addLast(node(s))
+            i1 = i1.next
+
+    if carry > 0:
+        result.addLast(node(carry))
+
+    return result
+
+def sumLists_recursive(l1: linkedList, l2: linkedList):
+    return linkedList(sumLists_helper(l1.head, l2.head, 0))
+
+def sumLists_helper(n1: node, n2: node, carry):
+    if n1 is None and n2 is None and carry == 0:
+        return None
+
+    result = node()
+    sum_val = carry
+
+    if n1 is not None:
+        sum_val += n1.data
+    if n2 is not None:
+        sum_val += n2.data
+
+    result.data = sum_val % 10
+    # recursive section
+    next1 = None
+    next2 = None
+    if n1 is not None:
+        next1 = n1.next
+    if n2 is not None:
+        next2 = n2.next
+
+    more = sumLists_helper(next1, next2, sum_val // 10)
+    result.next = more
+
+    return result
+
+l1 = linkedList(node(1))
+l1.add(node(5))
+l1.add(node(9))
+
+l2 = linkedList(node(9))
+l2.add(node(0))
+l2.add(node(2))
+l2.add(node(2))
+l2.add(node(9))
+
+l1.print()
+l2.print()
+sumLists(l1, l2).print()
+sumLists_recursive(l1, l2).print()
 # endregion
