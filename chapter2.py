f952e3a82a7d4af58e7c6b35f4860a51798a6360
Vahid-Esmaeelzadeh <v.esmaeelzadeh@gmail.com>
2019-06-06 20:29:25 -0700

    The follow up of Question 2.5 is added.

--- chapter2.py
+++ chapter2.py
@@ -269,18 +269,78 @@ def sumLists_helper(n1: node, n2: node, carry):
 
     return result
 
-l1 = linkedList(node(1))
-l1.add(node(5))
-l1.add(node(9))
+
+class partialSum:
+    sum_list: node = None  # to keep the linked list of partial sum
+    carry = 0
+
+
+def addLists(l1: linkedList, l2: linkedList):
+    diff = l1.size() - l2.size()
+    if diff > 0:
+        l2 = padList(l2, diff)
+    if diff < 0:
+        l1 = padList(l1, -diff)
+
+    n1 = l1.head
+    n2 = l2.head
+
+    # add lists
+    pSum:partialSum = addLists_helper(n1, n2)
+
+    if pSum.carry == 0:
+        return linkedList(pSum.sum_list)
+    else:  # insert a node for carry at the first of linked list
+        result = insertFirst(pSum.sum_list, pSum.carry)
+        return linkedList(result)
+
+
+def addLists_helper(n1: node, n2: node):
+    if n1 is None and n2 is None:
+        pSum = partialSum()
+        return pSum
+
+    pSum = addLists_helper(n1.next, n2.next)
+
+    val = n1.data + n2.data + pSum.carry
+    n = node(val % 10)  # create a new node with the new value
+
+    # add the new node at first of list
+    if pSum.sum_list is not None:
+        n.next = pSum.sum_list
+    pSum.sum_list = n
+
+    pSum.carry = val // 10
+    return pSum
+
+
+def insertFirst(n: node, val):
+    newNode = node(val)
+    if n is not None:
+        newNode.next = n
+    return newNode
+
+def padList(lst: linkedList, padding):
+    head = lst.head
+    for i in range(padding):
+        head = insertFirst(head, 0)
+
+    return linkedList(head)
+
+l1 = linkedList(node(7))
+l1.add(node(3))
+l1.add(node(2))
 
 l2 = linkedList(node(9))
+l2.add(node(3))
 l2.add(node(0))
-l2.add(node(2))
-l2.add(node(2))
+l2.add(node(6))
 l2.add(node(9))
 
 l1.print()
 l2.print()
 sumLists(l1, l2).print()
 sumLists_recursive(l1, l2).print()
+
+addLists(l1, l2).print()
 # endregion
