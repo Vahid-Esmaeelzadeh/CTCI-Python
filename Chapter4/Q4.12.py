9050e9c2601b40514694d0fdd7d55f7ef49f14a4
Vahid-Esmaeelzadeh <v.esmaeelzadeh@gmail.com>
2019-06-18 11:39:56 -0700

    The second solution of Q4.12 is added.

--- Chapter4/Q4.12.py
+++ Chapter4/Q4.12.py
@@ -28,7 +28,6 @@ def countPathsWithSum(root: Node, targetSum: int) -> int:
     pathsOnRight = countPathsWithSum(root.right, targetSum)
 
     return pathsFromRoot + pathsOnLeft + pathsOnRight
-
 def countPathsWithSumFromNode(n: Node, targetSum: int, currentSum: int) -> int:
     if not n:
         return 0
@@ -44,6 +43,36 @@ def countPathsWithSumFromNode(n: Node, targetSum: int, currentSum: int) -> int:
 
     return totalPaths
 
+
+def countPathsWithSum2(root: Node, targetSum: int) -> int:
+    return countPathsWithSum_rcr(root, targetSum, 0, dict())
+def countPathsWithSum_rcr(n: Node, targetSum: int, runningSum: int, pathCount: dict) -> int:
+    if not n:
+        return 0
+
+    runningSum += n.data
+    s = runningSum - targetSum
+    totalPaths = 0
+
+    if s in pathCount:
+        totalPaths = pathCount[s]
+
+    if runningSum == targetSum:
+        totalPaths += 1
+
+    if runningSum in pathCount:
+        pathCount[runningSum] += 1
+    else:
+        pathCount[runningSum] = 1
+
+    totalPaths += countPathsWithSum_rcr(n.left, targetSum, runningSum, pathCount)
+    totalPaths += countPathsWithSum_rcr(n.right, targetSum, runningSum, pathCount)
+
+    pathCount[runningSum] -= 1
+
+    return totalPaths
+
+
 root = Node(20)
 root.insertInOrder(10)
 root.insertInOrder(4)
@@ -56,3 +85,5 @@ root.insertInOrder(0)
 root.insertInOrder(7)
 
 print(countPathsWithSum(root, 16))
+print(countPathsWithSum2(root, 16))
+
